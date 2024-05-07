fcfebf629e369c09b52ddd95c03969cd4eb66c74
Ran Zalmon <ran.z@helloflare.com>
2024-05-07 11:25:04 +0300

    added browser and device detection to welcome page

---
+++ app/ui/user-device-info.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { UAParser } from 'ua-parser-js';
+
+export const UserDeviceInfo = () => {
+  const { browser, device } = UAParser(navigator.userAgent);
+
+  return (
+    <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
+      Browser Name: {browser.name}
+      <br />
+      Device Vendor: {device.vendor}
+      <br />
+      Device Model: {device.model}
+    </p>
+  );
+};
