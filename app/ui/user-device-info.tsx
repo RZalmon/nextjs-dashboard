ce0c5e7118e94ac13db3e07a12bd48fe4a6af5f3
Ran Zalmon <ran.z@helloflare.com>
2024-05-07 11:40:41 +0300

    fixed window object error

--- app/ui/user-device-info.tsx
+++ app/ui/user-device-info.tsx
@@ -1,8 +1,22 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { UAParser } from 'ua-parser-js';
 
 export const UserDeviceInfo = () => {
-  const { browser, device } = UAParser(window.navigator.userAgent);
+  const [browserInfo, setBrowserInfo] = useState<any>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const { browser, device } = UAParser(window.navigator.userAgent);
+      setBrowserInfo({ browser, device });
+    }
+  }, []);
+
+  if (!browserInfo) {
+    return <p>Loading...</p>;
+  }
+
+  const { browser, device } = browserInfo;
 
   return (
     <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
